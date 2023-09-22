8c8aefc0e2093f7e28365fc1f7ad5550ee4916bb
adriano lima rossi <adriano.limarossi@gmail.com>
2023-09-22 19:09:36 -0300

    IDK!

--- static/index.js
+++ static/index.js
@@ -1,4 +1,10 @@
-console.log("Version 1, but updated!")
+console.log("Version 1, but updated!");
+
+for(let i = 0; i < 40; i++){
+
+  console.log("Fucking stupid Loop!");
+
+};
 // New version 1...
 
 
